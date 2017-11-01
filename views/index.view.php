891d0820866fb0c50136e92d27782efc441713d1
headcruser <headcruser@gmail.com>
2017-11-01 00:34:37 -0600

    trabajo de estilos del navbar

--- views/index.view.php
+++ views/index.view.php
@@ -4,22 +4,27 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title>Blog</title>
 	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
 </head>
 <body>
 	<header>
 		<div class="contenedor">
-			<div class="logo izquierdo">
+			<div class="logo izquierda">
 				<p><a href="#"> Mi primer Blog</a></p>
 			</div>
 			<div class="derecha">
 				<form name="busqueda" class="buscar" action="" method="get">
-					<input type="text" name="busqeda" placeholder="Buscar">
-					<button type="submit" class="icono">buscar</button>
+					<input type="text" name="busqueda" placeholder="Buscar">
+					<button type="submit" class="icono fa fa-search"></button>
 				</form>
+
 				<nav class="menu">
 					<ul>
-						<li><a href=""> <i class=""></i> </a></li>
-						<li><a href=""> <i class=""></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i> </a></li>
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#">Contacto<i class="icono fa fa-envelope"></i></a></li>
 					</ul>
 				</nav>
 			</div>
